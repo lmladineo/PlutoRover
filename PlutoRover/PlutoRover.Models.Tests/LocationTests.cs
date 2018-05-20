bb8277ab65fb9d96bdbba503445f928468a6fa0a
Lea Mladineo <lea.mladineo@gmail.com>
2018-05-20 10:10:01 +0200

    RoverPluto.Models: Moving forward with start position facing any direction implemented

--- PlutoRover/PlutoRover.Models.Tests/LocationTests.cs
+++ PlutoRover/PlutoRover.Models.Tests/LocationTests.cs
@@ -36,7 +36,7 @@ namespace PlutoRover.Models.Tests
         }
 
         [Test]
-        public void CalculateNewLocation_SendForwardCommand_RoverMovesForward()
+        public void CalculateNewLocation_SendForwardCommandWithRoverFacingNorth_RoverMovesForwardWithRoverFacingNorth()
         {
             // Arrange
             var command = "F";
@@ -52,6 +52,64 @@ namespace PlutoRover.Models.Tests
                 .Match<Location>(location => location.Y == 1)
                 .And
                 .Match<Location>(location => location.Direction == Direction.N);
+        }
+
+        [Test]
+        public void CalculateNewLocation_SendForwardCommandWithRoverFacingEast_RoverMovesForwardWithRoverFacingEast()
+        {
+            // Arrange
+            var command = "F";
+            var currentLocation = new Location(0, 0, Direction.E);
+
+            // Act
+            var newLocation = currentLocation.CalculateNewLocation(command);
+
+            // Assert
+            newLocation.Should()
+                .Match<Location>(location => location.X == 1)
+                .And
+                .Match<Location>(location => location.Y == 0)
+                .And
+                .Match<Location>(location => location.Direction == Direction.E);
+        }
+
+        [Test]
+        public void CalculateNewLocation_SendForwardCommandWithRoverFacingSouth_RoverMovesForwardWithRoverFacingSouth()
+        {
+            // Arrange
+            var command = "F";
+            var currentLocation = new Location(0, 0, Direction.S);
+
+            // Act
+            var newLocation = currentLocation.CalculateNewLocation(command);
+
+            // Assert
+            newLocation.Should()
+                .Match<Location>(location => location.X == 0)
+                .And
+                .Match<Location>(location => location.Y == -1)
+                .And
+                .Match<Location>(location => location.Direction == Direction.S);
+        }
+
+
+        [Test]
+        public void CalculateNewLocation_SendForwardCommandWithRoverFacingWest_RoverMovesForwardWithRoverFacingWest()
+        {
+            // Arrange
+            var command = "F";
+            var currentLocation = new Location(0, 0, Direction.W);
+
+            // Act
+            var newLocation = currentLocation.CalculateNewLocation(command);
+
+            // Assert
+            newLocation.Should()
+                .Match<Location>(location => location.X == -1)
+                .And
+                .Match<Location>(location => location.Y == 0)
+                .And
+                .Match<Location>(location => location.Direction == Direction.W);
         }
     }
 }
--- PlutoRover/PlutoRover.Models/Location.cs
+++ PlutoRover/PlutoRover.Models/Location.cs
@@ -14,10 +14,23 @@ namespace PlutoRover.Models
             Y = y;
             Direction = direction;
 
-            _commandToNewLocation = new Dictionary<string, Func<Location>>()
+            _commandToNewLocation = new Dictionary<string, Func<Location>>();
+
+            switch (direction)
             {
-                {"F", MoveForward}
-            };
+                case Direction.N:
+                    _commandToNewLocation.Add("F", MoveNorth);
+                    break;
+                case Direction.E:
+                    _commandToNewLocation.Add("F", MoveEast);
+                    break;
+                case Direction.S:
+                    _commandToNewLocation.Add("F", MoveSouth);
+                    break;
+                case Direction.W:
+                    _commandToNewLocation.Add("F", MoveWest);
+                    break;
+            }
         }
 
         public int X { get; }
@@ -30,6 +43,12 @@ namespace PlutoRover.Models
             ? _commandToNewLocation[command]()
             : throw new Exception("Command not supported");
 
-        private Location MoveForward() => new Location(X, Y + 1, Direction);
+        private Location MoveNorth() => new Location(X, Y + 1, Direction);
+
+        private Location MoveEast() => new Location(X + 1, Y, Direction);
+
+        private Location MoveSouth() => new Location(X, Y - 1, Direction);
+
+        private Location MoveWest() => new Location(X - 1, Y, Direction);
     }
 }
