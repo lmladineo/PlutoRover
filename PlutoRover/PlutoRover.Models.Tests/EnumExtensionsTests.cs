3daa2eaceb1a04d284e07f1cda29a1235c7179cf
Lea Mladineo <lea.mladineo@gmail.com>
2018-05-20 10:47:58 +0200

    PlutoRover.Models: EnumExtensions class for getting next and previous enum value (for changing direction) added

---
+++ PlutoRover/PlutoRover.Models.Tests/EnumExtensionsTests.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using PlutoRover.Models.Enums;
+using PlutoRover.Models.Extensions;
+
+namespace PlutoRover.Models.Tests
+{
+    [TestFixture]
+    public class EnumExtensionsTests
+    {
+        [TestCase(Direction.N, Direction.E)]
+        [TestCase(Direction.E, Direction.S)]
+        [TestCase(Direction.S, Direction.W)]
+        [TestCase(Direction.W, Direction.N)]
+        public void Next_DirectionEnum_Moves90DegreesClockwise(Direction startDirection, Direction expectedEndDirection)
+        {
+            // Arrange
+
+            // Act
+            var actualEndDirection = startDirection.Next();
+
+            // Assert
+            actualEndDirection.Should().Be(expectedEndDirection);
+        }
+
+        [TestCase(Direction.N, Direction.W)]
+        [TestCase(Direction.W, Direction.S)]
+        [TestCase(Direction.S, Direction.E)]
+        [TestCase(Direction.E, Direction.N)]
+        public void Previous_DirectionEnum_Moves90DegreesCounterClockwise(Direction startDirection, Direction expectedEndDirection)
+        {
+            // Arrange
+
+            // Act
+            var actualEndDirection = startDirection.Previous();
+
+            // Assert
+            actualEndDirection.Should().Be(expectedEndDirection);
+        }
+    }
+}
---
+++ PlutoRover/PlutoRover.Models/Extensions/EnumExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PlutoRover.Models.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static T Next<T>(this T src) where T : struct
+        {
+            if (!typeof(T).IsEnum) throw new ArgumentException($"Argument {typeof(T).FullName} is not an Enum");
+
+            var arr = (T[])Enum.GetValues(src.GetType());
+            var j = Array.IndexOf<T>(arr, src) + 1;
+            return (j == arr.Length) ? arr[0] : arr[j];
+        }
+
+        public static T Previous<T>(this T src) where T : struct
+        {
+            if (!typeof(T).IsEnum) throw new ArgumentException($"Argument {typeof(T).FullName} is not an Enum");
+
+            var arr = (T[])Enum.GetValues(src.GetType());
+            var j = Array.IndexOf<T>(arr, src) - 1;
+            return (j == -1) ? arr[arr.Length - 1] : arr[j];
+        }
+    }
+}
