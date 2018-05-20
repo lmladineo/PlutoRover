a841d56b5474d0e52e2ef61e4b5b1313f27bc09e
Lea Mladineo <lea.mladineo@gmail.com>
2018-05-20 10:56:41 +0200

    PlutoRover.Contracts: ILocationService added

---
+++ PlutoRover/PlutoRover.Contracts/ILocationService.cs
@@ -0,0 +1,13 @@
+﻿using PlutoRover.Models;
+
+namespace PlutoRover.Contracts
+{
+    // Note: Can use DB or some external service.
+    // Depending on if Rover has it's on DB or must contact external service for info on his location.
+    public interface ILocationService
+    {
+        Location GetCurrentLocation();
+
+        void UpdateLocation(Location newLocation);
+    }
+}
