5acca4771080154c3f90843d50dbe55284593471
Lea Mladineo <lea.mladineo@gmail.com>
2018-05-20 09:28:30 +0200

    PlutoRover.Models: Location model and Direction enum added

---
+++ PlutoRover/PlutoRover.Models/Enums/Direction.cs
@@ -0,0 +1,10 @@
+﻿namespace PlutoRover.Models.Enums
+{
+    public enum Direction
+    {
+        N,
+        E,
+        S,
+        W
+    }
+}
---
+++ PlutoRover/PlutoRover.Models/Location.cs
@@ -0,0 +1,20 @@
+﻿using PlutoRover.Models.Enums;
+
+namespace PlutoRover.Models
+{
+    public class Location
+    {
+        public Location(int x, int y, Direction direction)
+        {
+            X = x;
+            Y = y;
+            Direction = direction;
+        }
+
+        public int X { get; }
+
+        public int Y { get; }
+
+        public Direction Direction { get; }
+    }
+}
