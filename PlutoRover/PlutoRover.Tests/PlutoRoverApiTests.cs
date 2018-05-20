bae7ee0f6109794ebd7b11343484563f8b575bf6
Lea Mladineo <lea.mladineo@gmail.com>
2018-05-20 09:40:25 +0200

    PlutoRover.Tests: PlutoRoverApiTests class created. 2 tests for IPlutoRoerApi.Move() added. Tests are failing, because there is no implementation.

---
+++ PlutoRover/PlutoRover.Tests/PlutoRoverApiTests.cs
@@ -0,0 +1,51 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using PlutoRover.Contracts;
+using PlutoRover.Models;
+using PlutoRover.Models.Enums;
+
+namespace PlutoRover.Tests
+{
+    [TestFixture]
+    public class PlutoRoverApiTests
+    {
+        private IPlutoRoverApi _plutoRoverApi;
+
+        [SetUp]
+        protected void SetUp()
+        {
+            _plutoRoverApi = new PlutoRoverApi();
+        }
+
+        [Test]
+        public void Move_SendOneCommand_ResponseIsNotNull()
+        {
+            // Arrange
+            var command = "F";
+
+            // Act
+            var result = _plutoRoverApi.Move(command);
+
+            // Assert
+            result.Should().NotBeNull();
+        }
+
+        [Test]
+        public void Move_SendForwardCommand_RoverMovesForward()
+        {
+            // Arrange
+            var command = "F";
+
+            // Act
+            var result = _plutoRoverApi.Move(command);
+
+            // Assert
+            result.Should()
+                .Match<Location>(location => location.X == 0)
+                .And
+                .Match<Location>(location => location.Y == 1)
+                .And
+                .Match<Location>(location => location.Direction == Direction.N);
+        }
+    }
+}
