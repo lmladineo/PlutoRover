05744034921c8c6c6a9b8e598dd6bd19691c7065
Lea Mladineo <lea.mladineo@gmail.com>
2018-05-20 10:22:42 +0200

    PlutoRover.Contracts: IValidationService.ValidateCommand() added

--- PlutoRover/PlutoRover.Contracts/IValidationService.cs
+++ PlutoRover/PlutoRover.Contracts/IValidationService.cs
@@ -1,6 +1,14 @@
-﻿namespace PlutoRover.Contracts
+﻿using PlutoRover.Models.Exceptions;
+
+namespace PlutoRover.Contracts
 {
     public interface IValidationService
     {
+        /// <summary>
+        /// Validates command.
+        /// </summary>
+        /// <param name="command">Input command.</param>
+        /// <exception cref="ValidationException">Validation fails.</exception>
+        void ValidateCommand(string command);
     }
 }
